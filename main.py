a9293995b98c097a376bf780acb70b8a39c8362b
Murari Gubba <134187603+MurariGubba@users.noreply.github.com>
2025-11-19 06:22:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("hello world")
+import pandas as pd
+import serial
+import matplotlib.pyplot as plt
+import time
+
+com_port = "/dev/cu.usbmodem11201"
+baudrate = 9600
+filename = "arduino_data.csv"
+
+
+
+
+
+
+
+
+
+'''plt.ion()
+fig,ax = plt.subplots()
+file = "testdata.csv"
+chunk = 50
+
+df = pd.dataFrame()
+chunks = pd.read_csv(file, chunksize=chunk)
+'''
